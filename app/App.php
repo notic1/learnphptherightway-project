36776bef4448864e5a038b866ef0d1ca1c4458af
Tomas Būdvytis <tomas.budvytis@ku.lt>
2022-02-08 20:21:42 +0200

    wip

--- app/App.php
+++ app/App.php
@@ -1,5 +1,50 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 // Your Code
+
+function readFiles($dir)
+{
+    if (!is_dir($dir)) {
+        
+        return 'Passed directory does not exists or is a file.';
+    }
+
+    $files = scandir($dir);
+    $content = [];
+
+    foreach($files as $file) {
+        if (is_dir($file)) continue;
+        $currentFile = fopen($dir . $file, 'r');
+        $keys = fgetcsv($currentFile);
+        
+        while (($line = fgetcsv($currentFile)) !== false) {
+            $content[] = array_combine($keys, $line);
+        }
+    }
+
+    return $content;
+}
+
+function getValues($collection) 
+{
+    $income = 0;
+    $expenses = 0;
+
+    foreach ($collection as $values) {
+        $value = str_replace('$', '', $values['Amount']);
+        if ((float)$value > 0) {
+            $income += (float)$value;
+        } else {
+            $expenses += (float)$value;
+        }
+    }
+
+    return [(float)$income, (float)$expenses];
+}
+
+function getNet($income, $expenses)
+{
+    return ($income - $expenses);
+}
--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 $root = dirname(__DIR__) . DIRECTORY_SEPARATOR;
 
@@ -8,4 +8,11 @@ define('APP_PATH', $root . 'app' . DIRECTORY_SEPARATOR);
 define('FILES_PATH', $root . 'transaction_files' . DIRECTORY_SEPARATOR);
 define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
 
+require_once APP_PATH . 'App.php';
 /* YOUR CODE (Instructions in README.md) */
+
+$collection = readFiles(FILES_PATH);
+[$income, $expenses] = getValues($collection);
+$net = getNet($income, $expenses);
+
+include_once VIEWS_PATH . 'transactions.php';
